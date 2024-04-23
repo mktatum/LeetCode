49e36fb1be8ee93e5615353871633913f83d8220
Melanie Tatum <140862029+mktatum@users.noreply.github.com>
2024-04-23 11:18:28 -0400

    Time: 54 ms (39.28%) | Memory: 49.3 MB (9.76%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -8,8 +8,3 @@ var lengthOfLastWord = function(s) {
     len = arr.length -1 
     return (arr[len].length);
 };
-
-// len = arr.length -1 gets the index of the last element in the array 
-// because array indices start from 0. Since the last element's index 
-// is one less than the length of the array, arr.length - 1 
-// gives you that index.
