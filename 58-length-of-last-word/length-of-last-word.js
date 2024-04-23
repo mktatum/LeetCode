78505b063bebfcc1fe196c9ea0382ece61ad2c57
Melanie Tatum <140862029+mktatum@users.noreply.github.com>
2024-04-23 11:17:16 -0400

    Time: 49 ms (69.46%) | Memory: 48.8 MB (59.55%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,9 +5,6 @@
 var lengthOfLastWord = function(s) {
     remove_space=s.trim()
     arr = remove_space.split(" ")
-    len = arr.length -1 //gets the index of the last element in the array 
-        // because array indices start from 0. Since the last element's index 
-        // is one less than the length of the array, arr.length - 1 
-        // gives you that index.
+    len = arr.length -1 
     return (arr[len].length);
 };
